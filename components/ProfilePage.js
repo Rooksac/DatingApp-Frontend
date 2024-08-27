06ecd6344c5f1e34a5a7af0eb96c7b28a456099e
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-08-27 11:05:21 -0400

    personal profile

---
+++ components/ProfilePage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const ProfilePage = () => {
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+
+
+/*
+Full Name = First, Last Name
+age
+gender
+preferrance(Straight, Bi, )
+
+Job
+Education
+Religion
+Home Town?
+Looking For?
+
+
+prompts?
+
+..pull in profile picture... 
+
+*/
